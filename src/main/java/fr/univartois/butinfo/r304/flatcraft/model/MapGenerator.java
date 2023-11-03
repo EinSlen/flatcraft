efffc95d406ebe859c6c18159a521e34d0ccc481
aude_halipre <aude_halipre@ens.univ-artois.fr>
2023-11-03 12:21:45 +0100

    Modification de la classe MapGenerator pour implémenter l'interface correspondante

--- src/main/java/fr/univartois/butinfo/r304/flatcraft/model/MapGenerator.java
+++ src/main/java/fr/univartois/butinfo/r304/flatcraft/model/MapGenerator.java
@@ -3,7 +3,7 @@ package fr.univartois.butinfo.r304.flatcraft.model;
 import fr.univartois.butinfo.r304.flatcraft.model.map.SimpleGameMap;
 import fr.univartois.butinfo.r304.flatcraft.view.ISpriteStore;
 
-public class MapGenerator {
+public class MapGenerator implements IMapGenerator {
 
     private int width;
     private int height;
@@ -41,6 +41,33 @@ public class MapGenerator {
                 if(i<map.getSoilHeight()) map.setAt(i,j,cell.createSky() );
             }
         }
+        return map;
+    }
+
+    @Override
+    public void generateMap(int height, int width, CellFactory cell) {
+        if (map==null){
+            map = new SimpleGameMap(height,width,height/2);
+            for(int i = 0; i <height;i++){
+                for(int j = 0; j < width; j++){
+                    if (i == map.getSoilHeight()){
+                        map.setAt(i,j,cell.createSoilSurface());
+                    }
+                    if(i < map.getSoilHeight()){
+                        map.setAt(i,j, cell.createSky());
+                    }
+                    else{
+                        map.setAt(i,j,cell.createSubSoil());
+                    }
+                }
+            }
+        }else {
+            System.err.println("La map est déjà générée ! ");
+        }
+    }
+
+    @Override
+    public SimpleGameMap getMap() {
         return map;
     }
 }
