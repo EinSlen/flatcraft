c374c263fb82495f2fd1f93badfa9c7e7f8ef617
aude_halipre <aude_halipre@ens.univ-artois.fr>
2024-01-11 17:00:21 +0100

    Ajout du module-info

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module fr.univartois.butinfo.r304.flatcraft {
+    exports fr.univartois.butinfo.r304.flatcraft;
+
+    opens fr.univartois.butinfo.r304.flatcraft.controller to javafx.fxml;
+
+    requires javafx.fxml;
+    requires javafx.graphics;
+    requires java.sql;
+    requires transitive javafx.controls;
+}
