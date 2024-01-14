c178e83050957d951f5d9ac052ce073a6bf1b357
aude_halipre <aude_halipre@ens.univ-artois.fr>
2024-01-14 11:04:09 +0100

    Correction Sonarlint

--- src/main/java/fr/univartois/butinfo/r304/flatcraft/model/cellules/ResourceCellState.java
+++ src/main/java/fr/univartois/butinfo/r304/flatcraft/model/cellules/ResourceCellState.java
@@ -1,7 +1,6 @@
 package fr.univartois.butinfo.r304.flatcraft.model.cellules;
 
 import fr.univartois.butinfo.r304.flatcraft.model.movables.IMovable;
-import fr.univartois.butinfo.r304.flatcraft.model.movables.Player;
 import fr.univartois.butinfo.r304.flatcraft.model.resources.Resource;
 
 public class ResourceCellState implements CellState {
