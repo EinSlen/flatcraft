68d29cfc463879b6619f4d10ab6c7aa6eb3004c7
aude_halipre <aude_halipre@ens.univ-artois.fr>
2024-01-14 10:49:10 +0100

    Correction Sonarlint

--- src/main/java/fr/univartois/butinfo/r304/flatcraft/model/movables/mobs/strategy/lineaire/DeplacementLineare.java
+++ src/main/java/fr/univartois/butinfo/r304/flatcraft/model/movables/mobs/strategy/lineaire/DeplacementLineare.java
@@ -1,11 +1,10 @@
 package fr.univartois.butinfo.r304.flatcraft.model.movables.mobs.strategy.lineaire;
 import fr.univartois.butinfo.r304.flatcraft.model.movables.IMovable;
 import fr.univartois.butinfo.r304.flatcraft.model.movables.mobs.strategy.IDeplacement;
-import fr.univartois.butinfo.r304.flatcraft.model.movables.mobs.strategy.intelligent.DeplacementIntelligent;
 
 public class DeplacementLineare implements IDeplacement {
     private static DeplacementLineare instance;
-    private DeplacementLineare(){};
+    private DeplacementLineare(){}
 
     public static DeplacementLineare getInstance(){
         if(instance==null) instance = new DeplacementLineare();
