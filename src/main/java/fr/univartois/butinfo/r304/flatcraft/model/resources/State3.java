fb13dc2f007656388af4d357f98a7a169379649b
aude_halipre <aude_halipre@ens.univ-artois.fr>
2024-01-14 10:37:39 +0100

    Correction Sonarlint

--- src/main/java/fr/univartois/butinfo/r304/flatcraft/model/resources/State3.java
+++ src/main/java/fr/univartois/butinfo/r304/flatcraft/model/resources/State3.java
@@ -2,7 +2,6 @@ package fr.univartois.butinfo.r304.flatcraft.model.resources;
 
 public class State3 implements IState{
 
-    private final int hardness = 3;
     private final Resource resource;
 
     public State3(Resource resource) {
@@ -16,6 +15,6 @@ public class State3 implements IState{
 
     @Override
     public int getHardness() {
-        return this.hardness;
+        return 3;
     }
 }
